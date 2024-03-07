8b635763ec1f05c304e8cd5414e5ce1e9269ae4d
basra4ka <evgeha_ivanov_01@bk.ru>
2024-03-07 16:54:32 +0300

    laba

--- task2.pl
+++ task2.pl
@@ -3,7 +3,11 @@
 товар('Лейка', 100).
 товар('Водяной насос', 500).
 товар('Набор отверок', 200).
+товар('Товар', 500).
+товар('Ещё товар', 400).
 
+
+sum200(S):- priceslist(L), sum200(L, S1), S is S1.
 sum200([], 0).
 sum200([H|T], S) :- sum200(T, S1), H>200, S is H + S1, !.
 sum200([_|T], S) :- sum200(T, S1), S is 0 + S1.
@@ -13,5 +17,7 @@ maxprice([H|T], CM, M) :-  H > CM, maxprice(T, H, M1), M is M1, !.
 maxprice([_|T], CM, M) :-  maxprice(T, CM, M1), M is M1.
 maxprice([], CM, M):- M is CM.
 
-
+maxpricetov(N, P) :- priceslist(S), maxpricetov(S, X, Y), N = X, P = Y.
 maxpricetov(P, X, Y):-  maxprice(P, M), товар(N, M), X = N, Y = M.
+
+priceslist(S) :- findall(X, товар(_, X), L), S = L.
